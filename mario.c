a56f7230c6b616a61a21bed07bbc4340aa41c84c
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-26 23:39:13 +0000

    Sat, Apr 27, 2024, 1:39 AM +02:00

---
+++ mario.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int input;
+    do
+    {
+        input = get_int("Height : ");
+        printf("Type a number between 1 and 8 included\n");
+    }
+    while (input < 1 || input > 8);
+
+    for (int i=1; i<=input; i++)
+    {
+        printf("\n");
+        if(i<)
+    }
+}
