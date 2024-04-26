f464b7a29f5040687714628340f6edd2009df4f2
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-26 22:53:00 +0000

    Sat, Apr 27, 2024, 12:53 AM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -10,6 +10,14 @@ int main(void)
         printf("Type a number between 1 and 8 included\n");
     }
     while (input < 1 || input > 8);
+
+    for (int i=0; i<input; i++)
+    {
+        for (int j=0; j<input; j++)
+        {
+            
+        }
+    }
 }
 
 
