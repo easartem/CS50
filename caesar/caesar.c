a5cd286c163f85f93e2d96080c2411b925d2178d
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 20:54:31 +0000

    Sat, Apr 27, 2024, 10:54 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,7 +8,11 @@ int main(int argc, string argv[])
 {
     // Make sure program was run with just one command-line argument
 
+        // If not, print an error and main return 1
+
     // Make sure every character in argv[1] is a digit
+
+        // If not, print "Usage: ./caesar key" and main return 1
 
     // Convert argv[1] from a `string` to an `int`
 
