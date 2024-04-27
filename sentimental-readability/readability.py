1ad8bacf4e935c851f1058d497953012f7f8e14e
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 22:05:18 +0000

    Sun, Apr 28, 2024, 12:05 AM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,40 +1,28 @@
-from cs50 import get_int
+from cs50 import get_string
 
 def main():
     # Prompt the user for some text
-    text = get_int("Text: ")
+    text = get_string("Text: ")
 
     # Compute L and S for the Coleman-Liau index
-    L = (count_letters(text) / (float) count_words(text)) * 100
-    S = (count_sentences(text) / (float) count_words(text)) * 100
+    # L = (count_letters(text) / (float) count_words(text)) * 100
+    # S = (count_sentences(text) / (float) count_words(text)) * 100
 
     # Compute the Coleman-Liau index
-    index = 0.0588 * L - 0.296 * S - 15.8
+    # index = 0.0588 * L - 0.296 * S - 15.8
+    index = 4
     # Print the grade level by rounding the result
-    int result = round(index)
+    result = round(index)
     if (index < 1):
-    {
-        printf("Before Grade 1")
-    }
+        print("Before Grade 1")
     elif (index >= 16):
-    {
-        printf("Grade 16+")
-    }
+        print("Grade 16+")
     else:
-    {
-        printf("Grade %i", result)
-    }
+        print("Grade %i", result)
 
-def count_letters(string text)
-    int len = strlen(text)
-    int count = 0
-    for (int i = 0; i < len; i++)
-    {
-        if (isalpha(text[i]))
-        {
-            count++
-        }
-    }
+def count_words(text):
+    words = text.split(" ")
+    count = len(words)
     return count
 
 main()
