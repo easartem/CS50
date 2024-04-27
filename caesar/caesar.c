587f881c6d1bbba7c46e615f567a8cec82c71bdf
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 21:00:06 +0000

    Sat, Apr 27, 2024, 11:00 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,8 +7,11 @@
 int main(int argc, string argv[])
 {
     // Make sure program was run with just one command-line argument
-
-        // If not, print an error and main return 1
+    if (argc != 2)
+    {
+        printf("Enter only the key as argument in the command-line");
+        return 1;
+    }
 
     // Make sure every character in argv[1] is a digit
 
@@ -17,9 +20,15 @@ int main(int argc, string argv[])
     // Convert argv[1] from a `string` to an `int`
 
     // Prompt user for plaintext
-    string plaintext = get_string("plaintext: ")
+    string plaintext = get_string("plaintext:  ");
+    printf("\n");
 
     // For each character in the plaintext:
 
     // Rotate the character if it's a letter
+
+    // Output the result
+    printf("plaintext: ", ciphertext);
+    printf("\n");
+    return 0;
 }
