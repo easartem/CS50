af0aa17e5a410a6d33243408fc04ee79ea873035
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 20:14:05 +0000

    Sat, Apr 27, 2024, 10:14 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string text);
+int count_words(string text);
+
 int main(void)
 {
     // Prompt the user for some text
@@ -15,15 +18,23 @@ int main(void)
     // get number of sentences by splitting with " " "," "."
     int letter_per_word[];
     int letter_per_word[];
-    int L = sum / 100;
-    int S = sum / 100;
-
-where L is the average number of letters per 100 words in the text,
-and S is the average number of sentences per 100 words in the text.
-
+    int L = sum / 100; // average number of letters per 100 words
+    int S = sum / 100; // average number of sentences per 100 words
 
     // Compute the Coleman-Liau index
     index = 0.0588 * L - 0.296 * S - 15.8
 
     // Print the grade level
 }
+
+int count_letters(string text)
+{
+
+}
+
+int count_words(string text)
+{
+
+}
+
+main()
