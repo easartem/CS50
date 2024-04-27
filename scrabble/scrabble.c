01178833975f331fcd6b334c0b79795a3ebd6317
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 18:07:38 +0000

    Sat, Apr 27, 2024, 8:07 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,6 +6,8 @@
 int compute_score(string word);
 
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+char ALPHABET[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
 
 int main(void)
 {
@@ -34,6 +36,7 @@ int main(void)
 
 int compute_score(string word)
 {
+    word = 
     // Compute and return score for word
     return score
 }
