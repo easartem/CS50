cb4253392c7e8fb6d99ba3bd1ec7764c248cd4c3
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 14:31:00 +0000

    Sat, Apr 27, 2024, 4:31 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -9,14 +9,27 @@ def main():
 
     # Initialize the change number to 0
     # Calculate how many coins you should give customer
+    print("Initial nb ", dollars)
     quarters, dollars = calculate_cash(dollars, 0.25)
+    print("after quarter ", dollars)
     dimes, dollars = calculate_cash(dollars, 0.10)
+    print("after dimes ", dollars)
     nickels, dollars = calculate_cash(dollars, 0.05)
+    print("after nickels ", dollars)
     pennies, dollars = calculate_cash(dollars, 0.01)
+    print("after pennies ", dollars)
     # Sum the number of quarters, dimes, nickels, and pennies used
     sum = 0
     sum = quarters + dimes + nickels + pennies
     print(sum)
+    print(quarters)
+    print(dimes)
+    print(nickels)
+    print(pennies)
+
+
+
+
 
 
 def calculate_cash(dollars, type):
