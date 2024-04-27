e336b1d68e30c0e3085d2cdaf8324cf07727acf1
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 13:06:20 +0000

    Sat, Apr 27, 2024, 3:06 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,9 +1,16 @@
 from cs50 import get_int
 
 def main():
+    # Get the desired height
     print("entered")
     answer = get_int("Height: ")
     while((answer <= 0) | (answer > 8)):
         answer = get_int("Height: ")
+
+    # Construct the pyramide
+    for i in range(answer):
+        for j in range(answer):
+            print(" "*())
+            print-
 
 main()
