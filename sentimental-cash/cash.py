d6228c3ad8d4e8b57ed1777f4c21ab0904674038
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 14:16:08 +0000

    Sat, Apr 27, 2024, 4:16 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -7,8 +7,10 @@ def main():
     while (answer < 0):
         answer = get_float("Change: ")
 
-    #
-
+    # Initialize the change number to 0
+    # Calculate how many quarters you should give customer
+    quarters = calculate_quarters(cents)
+    cents = cents - (quarters * 25);
     # Construct the pyramide
     for i in range(1, answer+1):
         print(" "*(answer-i), end="")
@@ -18,3 +20,15 @@ def main():
 
 
 main()
+
+
+    // Calculate how many quarters you should give customer
+    int quarters = calculate_quarters(cents);
+
+    // Subtract the value of those quarters from cents (25c)
+    cents = cents - (quarters * 25);
+
+    // Calculate how many dimes you should give customer (10c)
+    int dimes = calculate_dimes(cents);
+    // Subtract the value of those dimes from remaining cents
+    cents = cents - (dimes * 10);
