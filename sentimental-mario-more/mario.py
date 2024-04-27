9c5692076c3da7b50171963db0385e54e7ad6b02
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 13:04:24 +0000

    Sat, Apr 27, 2024, 3:04 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 def main():
     print("entered")
     answer = get_int("Height: ")
-    while(answer < 0 | answer > 8):
+    while((answer <= 0) | (answer > 8)):
         answer = get_int("Height: ")
 
 main()
