117015b6eb0055de98ed6c51d32c53ea755a4632
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 11:50:10 +0000

    Sat, Apr 27, 2024, 1:50 PM +02:00

--- cash/cash.c
+++ cash/cash.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 
 int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
@@ -21,17 +24,27 @@ int main(void)
     cents = cents - (quarters * 25);
 
     // Calculate how many dimes you should give customer (10c)
+    int dimes = calculate_dimes(cents);
     // Subtract the value of those dimes from remaining cents
+    cents = cents - (dimes * 10);
 
     // Calculate how many nickels you should give customer (5c)
+    int nickels = calculate_quarters(cents);
     // Subtract the value of those nickels from remaining cents
+    cents = cents - (nickels * 5);
 
     // Calculate how many pennies you should give customer (1c)
+    int pennies = calculate_pennies(cents);
     // Subtract the value of those pennies from remaining cents
+    cents = cents - (pennies * 1);
 
     // Sum the number of quarters, dimes, nickels, and pennies used
+    int sum = 0;
+    sum = quarters + dimes + nickels + pennies;
     // Print that sum
+    printf("%d\n", sum);
 }
+
 
 int calculate_quarters(int cents)
 {
@@ -43,4 +56,43 @@ int calculate_quarters(int cents)
         cents = cents - 25;
     }
     return quarters;
+}
+
+
+int calculate_dimes(int cents)
+{
+    // Calculate how many dimes you should give customer
+    int dimes = 0;
+    while (cents >= 10)
+    {
+        dimes++;
+        cents = cents - 10;
+    }
+    return dimes;
+}
+
+
+int calculate_nickels(int cents)
+{
+    // Calculate how many nickels you should give customer
+    int nickels = 0;
+    while (cents >= 5)
+    {
+        nickels++;
+        cents = cents - 5;
+    }
+    return nickels;
+}
+
+
+int calculate_pennies(int cents)
+{
+    // Calculate how many pennies you should give customer
+    int pennies = 0;
+    while (pennies >= 1)
+    {
+        pennies++;
+        cents = cents - 1;
+    }
+    return pennies;
 }
