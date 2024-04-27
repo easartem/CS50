9ef330683035a130004ee6db37a982fb756f9f21
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 18:03:12 +0000

    Sat, Apr 27, 2024, 8:03 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,10 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-void print_row(int bricks, int blank);
+int compute_score(string word);
+
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
 {
     int input1 = get_string("Player 1: ");
     int input2 = get_string("Player 2: ");
+
+    // Compute the score of each word
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // Print the winner
+    if (score1 == score2)
+    {
+        printf("Tie!");
+    }
+    else if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else
+    {
+        printf("Player 2 wins!");
+    }
+}
+
+
+int compute_score(string word)
+{
+    // Compute and return score for word
+    return score
 }
