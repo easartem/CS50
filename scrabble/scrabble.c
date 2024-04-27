54f01bf5117f0c9047bc03b4830c499d530d8fc7
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 18:36:40 +0000

    Sat, Apr 27, 2024, 8:36 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,13 +1,13 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 
 int compute_score(string word);
 
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10, 0};
-char ALPHABET[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
+char ALPHABET[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 int main(void)
 {
@@ -34,18 +34,17 @@ int main(void)
     printf("\n");
 }
 
-
 int compute_score(string word)
 {
     int score = 0;
     int lenword = strlen(word);
-    for (int i=0 ; i<lenword ; i++)
+    for (int i = 0; i < lenword; i++)
     {
         word[i] = tolower(word[i]);
     }
 
     // Compute and return score for word
-    for (int j=0 ; j<lenword ; j++)
+    for (int j = 0; j < lenword; j++)
     {
         int index = -1;
         do
@@ -57,7 +56,8 @@ int compute_score(string word)
             {
                 break;
             }
-        } while(word[j] != ALPHABET[index]);
+        }
+        while (word[j] != ALPHABET[index]);
         score = score + POINTS[index];
     }
     return score;
