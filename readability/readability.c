2762a39988e78c9151d6471e155aa2db0feff25c
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 20:11:37 +0000

    Sat, Apr 27, 2024, 10:11 PM +02:00

---
+++ readability/readability.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt the user for some text
+    string text = get_string("Text: ");
+
+    // Count the number of letters, words, and sentences in the text
+
+    // get array of words by splitting with " " "," "."
+    // get number of sentences by splitting with " " "," "."
+    int letter_per_word[];
+    int letter_per_word[];
+    int L = sum / 100;
+    int S = sum / 100;
+
+where L is the average number of letters per 100 words in the text,
+and S is the average number of sentences per 100 words in the text.
+
+
+    // Compute the Coleman-Liau index
+    index = 0.0588 * L - 0.296 * S - 15.8
+
+    // Print the grade level
+}
