841373126ebbd72b89e69b4b39e9dbb1c6bc808d
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-27 21:09:41 +0000

    Sat, Apr 27, 2024, 11:09 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,29 +3,31 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
     // Make sure program was run with just one command-line argument
     if (argc != 2)
     {
-        printf("Enter only the key as argument in the command-line");
+        printf("Enter only the key as argument in the command-line\n");
         return 1;
     }
 
     // Make sure every character in argv[1] is a digit
-    int len = strlen(argv[1])
+    int len = strlen(argv[1]);
     for (int i = 0; i < len; i++)
     {
         if (isdigit(argv[1][i]) == 0)
         {
-            printf("Usage: ./caesar key");
+            printf("Usage: ./caesar key\n");
             return 1;
         }
     }
 
     // Convert argv[1] from a `string` to an `int`
-
+    int key = atoi(argv[1]);
+    printf("The key is : %i\n", key);
     // Prompt user for plaintext
     string plaintext = get_string("plaintext:  ");
     printf("\n");
@@ -35,7 +37,8 @@ int main(int argc, string argv[])
     // Rotate the character if it's a letter
 
     // Output the result
-    printf("plaintext: ", ciphertext);
+    //printf("plaintext: %s", ciphertext);
+    printf("plaintext: ");
     printf("\n");
     return 0;
 }
