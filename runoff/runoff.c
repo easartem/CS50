65c1b9c3346ac754d7b10844d1ddfbfc2192c8df
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-28 17:13:07 +0000

    Sun, Apr 28, 2024, 7:13 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -137,7 +137,6 @@ bool vote(int voter, int rank, string name)
         if (strcmp(candidates[i].name, name) == 0)
         {
             preferences[voter][rank] = i;
-
             return true;
         }
     }
