0aa1afe6f2d88d2051321fc19049736118dcdfeb
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-28 17:04:42 +0000

    Sun, Apr 28, 2024, 7:04 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,6 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
     // find name, access attribute and add1
     for (int i = 0; i < candidate_count; i++)
     {
@@ -142,7 +141,6 @@ bool vote(int voter, int rank, string name)
         }
     }
     // no name
-
     return false;
 }
 // -----------------------------------------------------------------------------------------------------
