5ddd20f46b728222e374ffc3436dee6eae8e6e8d
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-29 22:04:04 +0000

    Tue, Apr 30, 2024, 12:04 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("There can be only one command-line argument.\n");
+        return 2;
+    }
 }
