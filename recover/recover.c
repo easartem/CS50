6433fd37f08d28afc99c61887e1e165daf6b145f
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-29 22:06:54 +0000

    Tue, Apr 30, 2024, 12:06 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,6 +6,12 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("There can be only one command-line argument.\n");
+        return 1;
+    }
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
         return 2;
     }
 }
