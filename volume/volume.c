a3069db026e43524a21d2378bc4a981c9ff12d0e
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-29 11:08:29 +0000

    Mon, Apr 29, 2024, 1:08 PM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,9 +40,9 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int size_int16 = 2; // sizeof(int16_t)
-    int16_t buffer; // Create a buffer for a single sample
+    int16_t buffer;     // Create a buffer for a single sample
     printf("%lu", sizeof(int16_t));
-    while(fread(&buffer, size_int16, 1, input))
+    while (fread(&buffer, size_int16, 1, input))
     {
         buffer = buffer * factor;
         fwrite(&buffer, size_int16, 1, output);
