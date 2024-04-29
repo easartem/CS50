5fb4dcd406fabefeb796466776d23b3437f2a4fe
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-29 11:46:44 +0000

    Mon, Apr 29, 2024, 1:46 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -20,15 +20,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    int sepiaRed;
+    RGBTRIPLE px;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            avg = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
-            image[i][j].rgbtBlue = avg;
-            image[i][j].rgbtGreen = avg;
-            image[i][j].rgbtRed = avg;
+            px.rgbtRed = (0.393 *  image[i][j].rgbtRed) + (0.769 *  image[i][j].rgbtGreen) + (0.189 *  image[i][j].rgbtBlue);
+            px.rgbtGreen = (0.349 *  image[i][j].rgbtRed) + (0.686 *  image[i][j].rgbtGreen) + (0.168 *  image[i][j].rgbtBlue);
+            px.rgbtBlue = (0.272 *  image[i][j].rgbtRed) + (0.534 *  image[i][j].rgbtGreen) + (0.131 *  image[i][j].rgbtBlue);
+            image[i][j].rgbtBlue = round(px.rgbtRed);
+            image[i][j].rgbtGreen = round(px.rgbtGreen);
+            image[i][j].rgbtRed = round(px.rgbtBlue);
         }
     }
     return;
