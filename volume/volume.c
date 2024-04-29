53d87f1a2b49ddc0e9107376e78fbe7452fb397e
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-29 00:21:46 +0000

    Mon, Apr 29, 2024, 2:21 AM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -39,7 +39,10 @@ int main(int argc, char *argv[])
     fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
-
+    // Create a buffer for a single sample
+    int16_t buffer;
+    fread(&buffer, sizeof(int16_t), 1, input)
+    // modify by factor
 
     // Close files
     fclose(input);
