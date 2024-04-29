0b3fd1baf4fbee56426c2ce508eb78dec9297b3e
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-29 11:42:23 +0000

    Mon, Apr 29, 2024, 1:42 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,9 +3,6 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    // take avg of each px(rgb) and cast it to all have the same (be careful to round the avg to int)
-    // array [row]
-    // array [row][col] gives pixel
     int avg;
     for (int i = 0; i < height; i++)
     {
@@ -23,6 +20,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int sepiaRed;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            avg = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtRed = avg;
+        }
+    }
     return;
 }
 
