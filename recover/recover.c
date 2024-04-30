cda9bbe86cfa0e7fa7083dfade5e0bcec8156479
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-30 11:56:42 +0000

    Tue, Apr 30, 2024, 1:56 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,21 +14,23 @@ int main(int argc, char *argv[])
         printf("Could not open file.\n");
         return 2;
     }
-    a program that iterates over a copy of my memory card, looking for JPEGs’ signatures.
-    Each time you find a signature, you can open a new file for writing
-     and start filling that file with bytes from my memory card,
-     closing that file only once you encounter another signature. Moreover,
-     rather than read my memory card’s bytes one at a time,
-     you can read 512 of them at a time into a buffer for efficiency’s sake.
      Thanks to FAT, you can trust that JPEGs’ signatures will be “block-aligned.”
      That is, you need only look for those signatures in a block’s first four bytes.
-      I’ve only imaged the first few megabytes of the memory card. But you should ultimately find that the image contains 50 JPEGs.
+        But you should ultimately find that the image contains 50 JPEGs.
     // Open memory card
+    FILE *f = fopen(argv[1], "r");
     // Repeat until the end of card:
+    while ()
+    {
+        
+    }
         // Read 512 bytes into a buffer
         // If start of new JPEG (each JPEG starts with a distinct header)
     first byte of every jpeg file is 0xff second is 0xd8 third 0xff
     the fourth one can vary (0xe0, 0xe1, 0xe2, ..., oxef) but always start with 0xe + some char
+    open a new file for writing
+    start filling that file with bytes from my memory card
+    closing that file only once you encounter another signature
             // If first JPEG
                 // ...
             // Else // JPEGs are stored back-to-back in memory card
