85ee754295090771c396cfc58327a5c4670f4d9c
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-30 16:05:04 +0000

    Tue, Apr 30, 2024, 6:05 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,8 +49,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        newperson -> parents[0] = parent0;
-        newperson -> parents[1] = parent1;
+        newperson->parents[0] = parent0;
+        newperson->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         int r1 = rand() % 2;
@@ -88,6 +88,7 @@ void free_family(person *p)
     {
         free_family(p->parents[0]);
         free_family(p->parents[1]);
+        free(p);
     }
     // TODO: Free child
     else
