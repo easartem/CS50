100c7e991f5f7e332fa38fbc8edaab55348c8898
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-30 15:51:22 +0000

    Tue, Apr 30, 2024, 5:51 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -84,7 +84,7 @@ void free_family(person *p)
         return;
     }
     // TODO: Free parents recursively
-    else if ()
+    else if (p->parents[0] != NULL)
     {
         free_family(p->parents[0]);
         free_family(p->parents[1]);
@@ -92,7 +92,7 @@ void free_family(person *p)
     // TODO: Free child
     else
     {
-        free(p)
+        free(p);
     }
 }
 
