2c7c1338fcce0c93d81428b2cc09837dea1969e6
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-04-30 12:15:46 +0000

    Tue, Apr 30, 2024, 2:15 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,52 +3,46 @@
 
 const int HEADER_SIZE = 4;
 const int BLOCK_SIZE = 512;
+const int JPEG_NB = 50;
 
 
 int main(int argc, char *argv[])
 {
+    char buffer[BLOCK_SIZE];
+    int jpgcpt = 0;
+
     if (argc != 2)
     {
         printf("There can be only one command-line argument.\n");
         return 1;
     }
+
+    // Open memory card
     FILE *f = fopen(argv[1], "r");
     if (f == NULL)
     {
         printf("Could not open file.\n");
         return 2;
     }
-     Thanks to FAT, you can trust that JPEGs’ signatures will be “block-aligned.”
-     That is, you need only look for those signatures in a block’s first four bytes.
-        But you should ultimately find that the image contains 50 JPEGs.
-    // Open memory card
-    FILE *f = fopen(argv[1], "r");
-    // Repeat until the end of card:
-    char buffer[BLOCK_SIZE];
-    // Read 512 bytes into a buffer
+
+    // Read 512 bytes into a buffer and repeat until the end of card:
     while (fread(*buffer, BLOCK_SIZE, 1, f))
     {
         // If start of new JPEG (each JPEG starts with a distinct header)
-        if (buffer[0] == 0xff)
-        buffer[0] == 0xff
-        buffer[1] == 0xd8
-        buffer[2] == 0xff
-        (buffer[3] & 0xf0) == 0xe0 + ? 16 different values
-            // If first JPEG
-    }
-        // If start of new JPEG (each JPEG starts with a distinct header)
-    first byte of every jpeg file is 0xff second is 0xd8 third 0xff
-    the fourth one can vary (0xe0, 0xe1, 0xe2, ..., oxef) but always start with 0xe + some char
-    open a new jpeg file for writing
-    start filling that file with bytes from my memory card
-    closing that file only once you encounter another signature
+        if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] & 0xf0) == 0xe0)
+        {
+            // make a new file
+            // filenames : ###.jpg starting from 000.jpg
             // If first JPEG
                 // ...
             // Else // JPEGs are stored back-to-back in memory card
                 // keep moving on, one block at a time, until you find one
+        }
         // Else
             // If already found JPEG
                 // ...
-
-
+    }
+    open a new jpeg file for writing
+    start filling that file with bytes from my memory card
+    closing that file only once you encounter another signature
 }
