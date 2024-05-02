8dbef71c18dd00f44ffa76076ef0a87f1a6a629c
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-02 14:54:17 +0000

    Thu, May 2, 2024, 4:54 PM +02:00

--- speller/dictionary.h
+++ speller/dictionary.h
@@ -8,6 +8,11 @@
 // Maximum length for a word
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
 
 // Prototypes
 bool check(const char *word);
