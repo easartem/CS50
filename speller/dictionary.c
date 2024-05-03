5e649ea83bbb2ce3dce01224d32762486bb8618d
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-03 14:15:34 +0000

    Fri, May 3, 2024, 4:15 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -61,11 +61,14 @@ bool load(const char *dictionary)
     char letter = 65;
     for (int i = 0; i < N; i++)
     {
-        printf("%c  %d\n", letter, letter);
-        node *base = malloc(sizeof(node));
-        base->word[0] = letter;
-        base->next = NULL;
-        table[i] = base;
+        // printf("%c  %d\n", letter, letter);
+        // node *base = malloc(sizeof(node));
+        // base->word[0] = letter;
+        // base->next = NULL;
+        // table[i] = base;
+        // letter = letter + 1;
+        table[i]->word[0] = letter;
+        table[i]->next = NULL;
         letter = letter + 1;
     }
 
