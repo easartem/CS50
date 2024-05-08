1ba637f2c13fd9a936d483ad9badd65d4ffd0c73
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-08 22:51:39 +0000

    Thu, May 9, 2024, 12:51 AM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,10 +1,7 @@
-SELECT title FROM movies WHERE id IN
-    (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Bradley Cooper')
-                                      AND person_id = (SELECT id FROM people WHERE name = 'Jennifer Lawrence'));
 
 
 SELECT title FROM movies WHERE id IN
     (SELECT movie_id FROM stars WHERE person_id IN
         (SELECT id FROM people WHERE name = 'Bradley Cooper' OR name = 'Jennifer Lawrence'));
 
-SELECT xx FROM 
+SELECT SELECT title FROM movies JOIN 
