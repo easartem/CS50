24589330fbe701f22297926c746b20d14299280e
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-08 23:53:15 +0000

    Thu, May 9, 2024, 1:53 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -27,3 +27,14 @@ SELECT name, transcript FROM interviews WHERE year = '2023' AND month = '7' AND 
 -------- Raymond : check the phone_calls, flights and bank_accounts. The thief called someone less than 1 min between 10:15am and 10:25am.
 -------- Raymond : check the flights and bank_accounts. He asked his collaborator to book him the earliest flight out of town tomorrow (29 july 2023)
 
+-- .schema bakery_security_logs(#id, year, month, day, hour, minute, activity, license_plate)
+-- we look for the car's license plate in the timeframe given by Ruth
+SELECT COUNT(*) FROM bakery_security_logs WHERE year = '2023' AND month = '7' AND day = '28' AND hour = '10' AND (minute >= 15 AND minute <= 25);
+-- 8 people left during that timeframe
+SELECT activity, license_plate FROM bakery_security_logs WHERE year = '2023' AND month = '7' AND day = '28' AND hour = '10' AND (minute >= 15 AND minute <= 25);
+
+
+-- .schema atm_transactions(id, account_number, year, month, day, atm_location, transaction_type, amount)
+-- we
+SELECT account_number, transaction_type, amount FROM atm_transactions WHERE year = '2023' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street';
+-- 1 withdraw and 8 deposit were made 
