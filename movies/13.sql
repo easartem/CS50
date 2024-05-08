200a7e05e5140c7c0a3e89a579f7e9e13ec5c08e
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-08 23:13:37 +0000

    Thu, May 9, 2024, 1:13 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -7,4 +7,4 @@ SELECT name FROM people WHERE id IN
     (SELECT person_id FROM stars WHERE movie_id IN
         (SELECT movie_id FROM stars WHERE person_id =
             (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958')))
-    AND NOT ;
+    AND name NOT 'Kevin Bacon';
