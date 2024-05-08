3ece6c2baa3290a20d5a3a535de8b664f8de98ca
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-08 22:12:51 +0000

    Thu, May 9, 2024, 12:12 AM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,3 +1,4 @@
-SELECT name FROM people ORDER BY birth
-SELECT person_id FROM stars WHERE movie_id =
-    (SELECT id FROM movies WHERE year = '2004')
+SELECT name FROM people WHERE id =
+    (SELECT person_id FROM stars WHERE movie_id =
+        (SELECT id FROM movies WHERE year = '2004'))
+        ORDER BY birth;
