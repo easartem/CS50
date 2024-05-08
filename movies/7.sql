9b5690d3d11b4e50629ac4b02f8fa069a55291c6
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-08 22:00:08 +0000

    Thu, May 9, 2024, 12:00 AM +02:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,2 @@
-SELECT title, rating FROM movies ORDER BY title ASC JOIN ratings ON movies.id = ratings.movie_id WHERE year = '2010' ORDER BY rating DESC;
+SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = '2010'
+ORDER BY rating DESC;
