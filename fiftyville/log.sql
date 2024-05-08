5cd539911ec14e3e03ed5c03d239a4acf1b9355f
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-08 23:26:59 +0000

    Thu, May 9, 2024, 1:26 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- The theft took place on July 28, 2023 and that it took place on Humphrey Street.
+
+-- Get an idea of the organisation of the database
+-- airports, atm_transactions, bakery_security_logs, bank_accounts, crime_scene_reports
+-- flights, interviews, passengers, people, phone_calls
+.table
+
+-- crime_scene_reports(#id, year, month, day, street, description)
+.schema crime_scene_reports
+
+SELECT * FROM crime_scene_reports WHERE year = '2023' AND month = 'July' AND day = '28';
