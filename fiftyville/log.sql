210ea70812033dd0bb8f39ced42969561955d76c
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-08 23:34:21 +0000

    Thu, May 9, 2024, 1:34 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,4 +10,15 @@
 -- crime_scene_reports(#id, year, month, day, street, description)
 .schema crime_scene_reports
 
-SELECT * FROM crime_scene_reports WHERE year = '2023' AND month = 'July' AND day = '28';
+-- start from the crime scene report with the initial information (date and street)
+SELECT * FROM crime_scene_reports WHERE year = '2023' AND month = '7' AND day = '28' AND street = 'Humphrey Street';
+-- new information unlocked ! hour of crime 10:15am at the bakery + existence if three interviews by witnesses
+
+-- let's go look at the transcript from the witnesses in the interviews table
+.schema interviews
+-- interviews(id, name, year, month, day, transcript)
+-- There is 7 interviews but we are only interested by 3
+SELECT COUNT(*) FROM interviews WHERE year = '2023' AND month = '7' AND day = '28';
+
+SELECT name, transcript FROM interviews WHERE year = '2023' AND month = '7' AND day = '28';
+
