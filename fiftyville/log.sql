69af4842117c202f7cfbb13e6700ed96af4050f9
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-09 22:37:03 +0000

    Fri, May 10, 2024, 12:37 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -55,6 +55,8 @@ SELECT name, transcript
 
 
 ---------------------------------------------RUTH LEAD-----------------------------------------------------------------------
+---------------------------------------------EUGENE LEAD---------------------------------------------------------------------
+---------------------------------------------RAYMOND LEAD--------------------------------------------------------------------
 
 -- we look for the car's license plate in the timeframe given by Ruth
 SELECT COUNT(*) FROM bakery_security_logs WHERE year = '2023' AND month = '7' AND day = '28' AND hour = '10' AND (minute >= 15 AND minute <= 25);
