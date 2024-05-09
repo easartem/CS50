30f0df3a44a450d67eb8834581614ed6521f0745
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-09 22:09:29 +0000

    Fri, May 10, 2024, 12:09 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,6 +2,7 @@
 
 -- The only information we have to start is that the theft took place on July 28, 2023 on Humphrey Street.
 
+
 -- Since we have a database at our disposition to investigate, the first thing to do is to take a look at its global organisation.
 .table
 .schema
@@ -17,11 +18,13 @@
     -- people(id, name, phone_number, passport_number, license_plate)
     -- phone_calls(id, caller, receiver, year, month, day, duration)
 
+
 -- start from the crime scene report with the initial information (date and street)
 SELECT * FROM crime_scene_reports WHERE year = '2023' AND month = '7' AND day = '28' AND street = 'Humphrey Street';
 -- New information unlocked ! hour of crime 10:15am at the bakery + existence if three interviews by witnesses
 
 
+-----------------------------------------LAUNCHING THE INVESTIGATION-------------------------------------------
 
 -- let's go look at the transcript from the witnesses in the interviews table
 -- There is 7 interviews but we are only interested by 3
