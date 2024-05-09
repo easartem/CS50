967e2a4b5862b50b9ccb135d54d3d68564f862d9
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-09 00:32:45 +0000

    Thu, May 9, 2024, 2:32 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -84,9 +84,14 @@ SELECT * FROM people WHERE license_plate IN
     (SELECT license_plate FROM bakery_security_logs WHERE year = '2023' AND month = '7' AND day = '28' AND hour = '10' AND (minute >= 15 AND minute <= 25))
 INTERSECT
 SELECT * FROM people WHERE passport_number IN
-    (SELECT passport_number FROM passengers WHERE flight_id = '36');
-
+    (SELECT passport_number FROM passengers WHERE flight_id = '36')
+INTERSECT
+SELECT * FROM people WHERE id IN
+    (SELECT person_id FROM bank_accounts WHERE account_number IN
+        (SELECT account_number FROM atm_transactions WHERE year = '2023' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'));
 
 -- .schema atm_transactions(id, account_number, year, month, day, atm_location, transaction_type, amount)
-SELECT * FROM people WHERE account_number IN
-    (SELECT account_number FROM atm_transactions WHERE year = '2023' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw');
+SELECT * FROM people WHERE id IN
+    (SELECT person_id FROM bank_accounts WHERE account_number IN
+        (SELECT account_number FROM atm_transactions WHERE year = '2023' AND month = '7' AND day = '28' AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'));
+-- .schema bank_accounts(account_number, person_id, creation_year) person_id ref people(id)
