72a473e09797b3073a4049b82252567cc14264ca
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-09 00:26:03 +0000

    Thu, May 9, 2024, 2:26 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -70,7 +70,15 @@ SELECT license_plate FROM bakery_security_logs WHERE year = '2023' AND month = '
 SELECT * FROM people WHERE license_plate IN
     (SELECT license_plate FROM bakery_security_logs WHERE year = '2023' AND month = '7' AND day = '28' AND hour = '10' AND (minute >= 15 AND minute <= 25));
 
+
+
+
 SELECT * FROM phone_calls WHERE year = '2023' AND month = '7' AND day = '28' AND duration <= '60';
 
+
+
 SELECT * FROM people WHERE phone_number IN
-    (SELECT caller FROM phone_calls WHERE year = '2023' AND month = '7' AND day = '28' AND duration <= '60');
+    (SELECT caller FROM phone_calls WHERE year = '2023' AND month = '7' AND day = '28' AND duration <= '60')
+INTERSECT
+SELECT * FROM people WHERE license_plate IN
+    (SELECT license_plate FROM bakery_security_logs WHERE year = '2023' AND month = '7' AND day = '28' AND hour = '10' AND (minute >= 15 AND minute <= 25));
