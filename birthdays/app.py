ef7b45dd46299af0bff72494d91cfcfdbcee59b5
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-05-25 12:38:44 +0000

    Sat, May 25, 2024, 2:38 PM +02:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -14,7 +14,7 @@ db = SQL("sqlite:///birthdays.db")
 
 
 @app.after_request
-def after_request(response):  
+def after_request(response):
     """Ensure responses aren't cached"""
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
@@ -27,7 +27,11 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
         return redirect("/")
 
     else:
