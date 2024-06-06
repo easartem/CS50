627e09e8c937f419cc4dd2d34f4ce6045883a314
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-06-06 14:23:34 +0000

    Thu, Jun 6, 2024, 4:23 PM +02:00

--- project-admin-side/app.py
+++ project-admin-side/app.py
@@ -13,7 +13,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///../project/tattooshop.db")
+# db = SQL("sqlite:///../project/tattooshop.db")
 
 
 @app.after_request
