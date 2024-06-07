eec4d137d0a807d732495c733cffc01633b7bc42
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-06-07 16:15:45 +0000

    Fri, Jun 7, 2024, 6:15 PM +02:00

--- project-admin-side/app.py
+++ project-admin-side/app.py
@@ -27,3 +27,35 @@ def after_request(response):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/clients", methods=["GET"])
+def studio():
+    return render_template("client.html")
+
+@app.route("/rdv", methods=["GET"])
+def galerie():
+    return render_template("rdv.html")
+
+@app.route("/team", methods=["GET"])
+def studio():
+    return render_template("team.html")
+
+@app.route("/kpi", methods=["GET"])
+def galerie():
+    return render_template("kpi.html")
+
+@app.route("/finance", methods=["GET"])
+def studio():
+    return render_template("finances.html")
+
+@app.route("/artists", methods=["GET"])
+def galerie():
+    return render_template("artists.html")
+
+@app.route("/gallery", methods=["GET"])
+def studio():
+    return render_template("gallery.html")
+
+@app.route("/parameters", methods=["GET"])
+def galerie():
+    return render_template("parameters.html")
