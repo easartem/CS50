56fe2562ea8f3d863ef25b1cf5cfcba2a90f962b
Emeline Bagoris <58293676+easartem@users.noreply.github.com>
2024-06-15 22:43:16 +0000

    Sun, Jun 16, 2024, 12:43 AM +02:00

--- project/helpers.py
+++ project/helpers.py
@@ -1,1 +1,14 @@
-def()
+def initialize_table_studio():
+    studio_name = "Tattoo Studio"
+    studio_address = "00 rue du tatouage, Paris, France"
+    studio_telephone = "01 23 45 67 88"
+    studio_email = "info_studio@gmail.com"
+    studio_instagram = "url de l'instagram du studio"
+    studio_openhours = "10h-13h & 15h-20h"
+
+    try:
+        db.execute("INSERT INTO studio (name, address, telephone, email, instagram, openhours) VALUES (?, ?, ?, ?, ?, ?)",
+                    studio_name, studio_address, studio_telephone, studio_email, studio_instagram, studio_openhours)
+    except ValueError:
+        return 400
+    return
